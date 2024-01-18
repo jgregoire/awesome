b479f07fe43f406864e29ef5f7107c39c7a13fb3
james <jgregoire@tutamail.com>
2024-01-17 22:12:39 -0500

    Added more keybinds. Not ready to include yet.

--- new_binds.lua
+++ new_binds.lua
@@ -3,91 +3,119 @@ local hotkeys_popup = require('awful.hotkeys_popup').widget
 require('awful.hotkeys_popup.keys')
 local lain = require('lain')
 
-local meh   = { 'Mod1', 'Shift', 'Control' }
-local hyper = { 'Mod1', 'Mod4', 'Shift', 'Control' }
-local super = { 'Mod4' }
-local alt   = { 'Mod1' }
+local client_mod  = { 'Mod1' } -- Alt
+local client_mod2 = { 'Mod1', 'Shift' }
+local tag_mod     = { 'Mod4' } -- Super
+local tag_mod2    = { 'Mod4', 'Shift' }
+local layout_mod  = { 'Mod1', 'Shift', 'Control' } -- Meh key
+local screen_mod  = { 'Shift' }
 
 return {
-    -- Awesome
-    awful.key(super, 's', hotkeys_popup.showhelp, { description = 'show help', group = 'awesome' }),
-    awful.key(super, 'w',
-        function ()
-            awful.util.mymainmenu:show()
-        end,
-        { description = 'show main menu', group = 'awesome' }
-    ),
+    global = {
+        -- Global keys
+        {
+            layout_mod, 'w', function() awful.util.mymainmenu:show() end,
+            { description = 'show main menu', group = 'awesome' }
+        },
+        {
+            layout_mod, 's', hotkeys_popup.show_help,
+            { description = 'show help', group = 'awesome' },
+        },
+        -- Tag keys
+        {
+            tag_mod, 'Left', awful.tag.viewprev,
+            { description = 'view previous', group = 'tag' }
+        },
+        {
+            tag_mod, 'Right', awful.tag.viewnext,
+            { description = 'view next', group = 'tag' }
+        },
+        {
+            tag_mod, 'Escape', awful.tag.history_restore,
+            { description = 'go back', group = 'tag' }
+        },
+        {
+            tag_mod2, 'Left', function() lain.util.tag_view_nonempty(-1) end,
+            { description = 'view previous nonempty', group = 'tag' }
+        },
+        {
+            tag_mod2, 'Right', function() lain.util.tag_view_nonempty(1) end,
+            { description = 'view next nonempty', group = 'tag' }
+        },
+        -- Layout keys
+        {
+            layout_mod, 'Up', function() awful.client.swap.byidx(1) end,
+            { description = 'swap with next client by index', group = 'layout' },
+        },
+        {
+            layout_mod, 'Down', function() awful.client.swap.byidx(-1) end,
+            { description = 'swap with previous client by index', group = 'layout' },
+        },
+        -- Screen keys
+        {
+            screen_mod, 'Left', function() awful.screen.focus_relative(-1) end,
+            { description = 'focus previous screen', group = 'screen' },
+        },
+        {
+            screen_mod, 'Left', function() awful.screen.focus_relative(1) end,
+            { description = 'focus next screen', group = 'screen' },
+        },
+        -- Client keys
+        {
+            client_mod2, 'Up', function() awful.client.focus.byidx(1) end,
+            { description = 'focus next by index', group = 'client' },
+        },
+        {
+            client_mod2, 'Down', function() awful.client.focus.byidx(-1) end,
+            { description = 'focus previous by index', group = 'client' },
+        },
+        {
+            client_mod, 'Up',
+            function()
+                awful.client.focus.global_bydirection('up')
+                if client.focus then client.focus:raise() end
+            end,
+            { description = 'focus up', group = 'client' }
+        },
+        {
+            client_mod, 'Down',
+            function()
+                awful.client.focus.global_bydirection('down')
+                if client.focus then client.focus:raise() end
+            end,
+            { description = 'focus down', group = 'client' },
+        },
+        {
+            client_mod, 'Left',
+            function()
+                awful.client.focus.global_bydirection('left')
+                if client.focus then client.focus:raise() end
+            end,
+            { description = 'focus left', group = 'client' },
+        },
+        {
+            client_mod, 'Right',
+            function()
+                awful.client.focus.global_bydirection('right')
+                if client.focus then client.focus:raise() end
+            end,
+            { description = 'focus right', group = 'client' },
+        },
+        {
+            client_mod, 'u', awful.client.urgent.jumpto,
+            { description = 'jump to urgent client', group = 'client' },
+        },
+        {
+            client_mod, 'Tab',
+            function()
+                awful.client.focus.history.previous()
+                if client.focus then client.focus:raise() end
+            end,
+            { description = 'focus last client', group = 'client' },
+        }
 
-    -- Client
-    awful.key({ 'Mod1', 'Control' }, 'Left', 
-        function ()
-            awful.client.focus.byidx(1)
-        end, 
-        { description = 'focus prev by index', group = 'client' }
-    ),
-    awful.key({ 'Mod1', 'Control' }, 'Right',
-        function ()
-            awful.client.focus.byidx(-1)
-        end,
-        { description = 'focus next by index', group = 'client' }
-    ),
-    awful.key(alt, 'Left',
-        function ()
-           awful.client.focus.global_bydirection('left')
-           if client.focus then client.focus:raise() end
-        end,
-        { description = 'focus left', group = 'client' }
-    ),
-    awful.key(alt, 'Right',
-        function ()
-            awful.client.focus.global_bydirection('right')
-            if client.focus then client.focus:raise() end
-        end,
-        { description = 'focus right', group = 'client' }
-    ),
-    awful.key(alt, 'Up',
-        function ()
-            awful.client.focus.global_bydirection('up')
-            if client.focus then client.focus:raise() end
-        end,
-        { description = 'focus up', group = 'client' }
-    ),
-    awful.key(alt, 'Down',
-        function ()
-            awful.client.focus.global_bydirection('down')
-            if client.focus then client.focus:raise() end
-        end,
-        { description = 'focus down', group = 'client' }
-    ),
-    awful.key({ 'Mod1', 'Shift' }, 'Left',
-        function ()
-            awful.client.swap.byidx(1)
-        end,
-        { description = 'swap with next client by index', group = 'client' }
-    ),
-    awful.key({ 'Mod1', 'Shift' }, 'Right',
-        function ()
-            awful.client.swap.byidx(-1)
-        end,
-        { description = 'swap with prev client by index', group = 'client' }
-    ),
+    },
+    client = {
 
-    -- Tag
-    awful.key(super, 'Left',   awful.tag.viewprev,        { description = 'view previous', group = 'tag' }),
-    awful.key(super, 'Right',  awful.tag.viewnext,        { description = 'view next', group = 'tag'}),
-    awful.key(super, 'Escape', awful.tag.history.restore, { description = 'go back', group = 'tag' }),
-    awful.key({ 'Mod4', 'Shift' }, 'Left',
-        function () lain.util.tag_view_nonempty(-1) end,  { description = 'view prev nonempty', group = 'tag' }
-    ),
-    awful.key({ 'Mod4', 'Shift' }, 'Right',
-        function () lain.util.tag_view_nonempty(1) end,   { description = 'view next nonempty', group = 'tag' }
-    ),
-
-    -- Screen
-    awful.key(meh, 'Tab',
-        function ()
-            awful.screen.focus_relative(1)
-        end,
-        { description = 'focus next screen', group = 'screen' }
-    ),
+    },
 }
